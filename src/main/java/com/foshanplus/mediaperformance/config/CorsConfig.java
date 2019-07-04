61b3c9a2b6f9d932e41afbc2eee15553b01e8f35
JaloDeng <651379728@qq.com>
2019-07-04 17:19:46 +0800

    跨域设置

---
+++ src/main/java/com/foshanplus/mediaperformance/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.foshanplus.mediaperformance.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Jalo Deng
+ * @email 651379728@qq.com
+ * @date 2019-07-04 17:16
+ */
+
+public class CorsConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("*")
+			.allowedMethods("*")
+			.allowedHeaders("*")
+			.allowCredentials(true)
+			.maxAge(3600);
+	}
+	
+}
