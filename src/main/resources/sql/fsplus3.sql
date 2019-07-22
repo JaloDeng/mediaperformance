435d5c97ebf935b9600fefe7c6e7e279df99a771
JaloDeng <651379728@qq.com>
2019-07-22 18:00:09 +0800

    修改导出浏览量sql

--- src/main/resources/sql/fsplus3.sql
+++ src/main/resources/sql/fsplus3.sql
@@ -6,8 +6,9 @@ SELECT
 	,'clickCount'
 UNION
 SELECT 
-n.title, IFNULL(ne.clickcount, 0)
+n.title, IFNULL(MAX(ne.clickcount), 0) AS clickCount
 FROM t_news n
 INNER JOIN t_news_ext ne ON ne.newsid = n.id
-WHERE DATE_FORMAT(n.publishtime, '%Y-%m-%d') BETWEEN '2019-07-01' AND '2019-07-19'
+WHERE DATE_FORMAT(n.publishtime, '%Y-%m-%d') BETWEEN '2019-07-01' AND '2019-07-20'
+GROUP BY n.title
 ;
